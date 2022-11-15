c70262f950577681c3998134f19211ee17f8f3a3
EC2 Default User <ec2-user@ip-172-31-32-132.ap-northeast-1.compute.internal>
2022-11-15 11:56:16 +0000

    [finish]4-12

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,4 +4,13 @@ class PostImage < ApplicationRecord
 
   belongs_to :user
 
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_imafe.hpg')
+      image.attach(io: File.open(file_path), filename: 'default_image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
+
 end
